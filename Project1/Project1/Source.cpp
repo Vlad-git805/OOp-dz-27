cba16c79c5cb5acc28c65f299911469c35dadc0f
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2020-08-04 18:28:01 +0300

    Created a project. Created a class Machine with parameters.

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+class Car
+{
+private:
+	string name;
+	int year;
+	int engine_capacity;
+	int price;
+public:
+};
